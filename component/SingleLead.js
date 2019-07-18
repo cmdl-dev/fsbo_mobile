a29ae017edb03580cfb271bddc006c63dfd2c00b
cesarahernandez <cesarahernandez127@gmail.com>
2019-07-18 11:37:07 -0700

    There is an indicator when the user swipes left and right

--- component/SingleLead.js
+++ component/SingleLead.js
@@ -16,25 +16,49 @@ import Icon from "react-native-vector-icons/FontAwesome";
 
 const SingleLead = ({ leadInfo }) => {
   const leadXPos = new Animated.Value(0);
+  const nextPhaseOpacity = new Animated.Value(0);
+  const archiveOpacity = new Animated.Value(0);
+
   const leadPanResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (evt, gs) => {
       const width = Dimensions.get("window").width;
-      console.log(width, gs.dx);
-      leadXPos.setValue(gs.dx);
+      //Values for the lead container position
+      const leftThreshold = -1 * width * 0.5;
+      const rightThreshold = width * 0.5;
+      if (gs.dx > leftThreshold && gs.dx < rightThreshold) {
+        leadXPos.setValue(gs.dx);
+      }
+      //Values for the option opacity
+      nextPhaseOpacity.setValue(gs.dx / rightThreshold);
+      archiveOpacity.setValue(gs.dx / leftThreshold);
     },
     onPanResponderEnd: (evt, gs) => {
       const width = Dimensions.get("window").width;
-      if (gs.dx < -1 * width * 0.4 || gs.dx > width * 0.4) {
-        const swipeDirection = gs.dx < -1 * width * 0.4 ? "left" : "right";
+      const leftThreshold = -1 * width * 0.47;
+      const rightThreshold = width * 0.47;
+      if (gs.dx < leftThreshold || gs.dx > rightThreshold) {
+        const swipeDirection = gs.dx < -1 * width * 0.47 ? "left" : "right";
         //Swipe left
         Animated.timing(leadXPos, {
           toValue: swipeDirection === "left" ? -1 * width : 1 * width,
           duration: 250
         }).start();
-        console.log("ENDED");
+        if (swipeDirection === "left") {
+          console.log("Archive");
+        } else {
+          console.log("Next Phase");
+        }
       } else {
         Animated.timing(leadXPos, {
+          toValue: 0,
+          duration: 250
+        }).start();
+        Animated.timing(nextPhaseOpacity, {
+          toValue: 0,
+          duration: 250
+        }).start();
+        Animated.timing(archiveOpacity, {
           toValue: 0,
           duration: 250
         }).start();
@@ -45,23 +69,44 @@ const SingleLead = ({ leadInfo }) => {
     console.log("You are calling someone");
   };
   return (
-    <Animated.View
-      {...leadPanResponder.panHandlers}
-      style={[styles.container, { left: leadXPos }]}
-    >
-      <View style={styles.userInfo}>
-        <Text>{leadInfo.firstName}</Text>
-        <Text>{leadInfo.lastName}</Text>
+    <View style={styles.outerContainer}>
+      <Animated.View
+        {...leadPanResponder.panHandlers}
+        style={[styles.container, { left: leadXPos }]}
+      >
+        <View style={styles.userInfo}>
+          <Text>{leadInfo.firstName}</Text>
+          <Text>{leadInfo.lastName}</Text>
+        </View>
+        <TouchableOpacity onPress={_handleCall} style={styles.contactInfo}>
+          <Text>{leadInfo.phone}</Text>
+          <Icon style={styles.phone} name="phone" size={30} color="#900" />
+        </TouchableOpacity>
+      </Animated.View>
+      <View style={styles.options}>
+        <Animated.Text
+          style={[
+            styles.nextPhase,
+            styles.option,
+            { opacity: nextPhaseOpacity }
+          ]}
+        >
+          Next Phase
+        </Animated.Text>
+        <Animated.Text
+          style={[styles.archive, styles.option, { opacity: archiveOpacity }]}
+        >
+          Archive
+        </Animated.Text>
       </View>
-      <TouchableOpacity onPress={_handleCall} style={styles.contactInfo}>
-        <Text>{leadInfo.phone}</Text>
-        <Icon style={styles.phone} name="phone" size={30} color="#900" />
-      </TouchableOpacity>
-    </Animated.View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  outerContainer: {
+    width: "100%"
+  },
   container: {
     marginBottom: "10px",
     display: "flex",
@@ -71,7 +116,9 @@ const styles = StyleSheet.create({
     padding: "10px",
     justifyContent: "space-between",
     width: "100%",
-    height: "75px"
+    height: "75px",
+    position: "relative",
+    zIndex: 10
   },
   userInfo: {
     display: "flex",
@@ -83,6 +130,32 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center"
+  },
+  options: {
+    zIndex: 0,
+    width: "100%",
+    height: "86%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    position: "absolute",
+    alignItems: "center"
+  },
+  option: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  nextPhase: {
+    width: 80,
+    height: "100%",
+    backgroundColor: "green"
+  },
+  archive: {
+    width: 50,
+    height: "100%",
+    backgroundColor: "red"
   }
 });
 export default SingleLead;
